95c7ba0c5e3e87a0ba239408479099405e5acb36
FrankIllner <frankillner@web.de>
2024-05-23 22:28:50 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -1,34 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.indexOf("products") > -1) {
         let select = document.querySelectorAll('.select__select');
-        let priceRegular = document.querySelector('.price__container .price-item--regular');
-        let priceSale = document.querySelector('.product__info-wrapper');
-        let priceRange = document.querySelector('.price__container .price-range-example');
+        let productWapper = document.querySelector('.product__info-wrapper');
 
-        priceRegular.classList.add('foo');
-        priceRange.classList.add('foo2');
-        priceSale.classList.add('foo2');
         select.forEach(function (elem) {
             elem.addEventListener("change", function (event) {
                 var item = event.target.value;
-                console.log("Selected value:", item);
-
+            
                 if (item === "") {
-                    console.log('1');
-                    priceRange.classList.remove('hidden');
-                   
-                    priceRegular.style.display = 'none';
-                    priceSale.style.display = 'none';
-                
-                    console.log(priceRange);
+                    productWapper.classList.add("is-price-range");
                 } else {
-                    console.log('2');
-                    
-                    priceRange.classList.add('hidden');
-                   
-                    priceRegular.style.display = 'block';
-                    priceSale.style.display = 'block';
-                    console.log(priceRange);
+                    productWapper.classList.remove("is-price-range");
                 }
 
             });

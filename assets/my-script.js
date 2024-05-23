e169b4e4f750e16f53a25b15ceac1e7f0c7f03a4
FrankIllner <frankillner@web.de>
2024-05-23 14:10:22 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -4,35 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
       let priceRegular = document.querySelector('.price .price__container .price-item--regular');
       let priceSale = document.querySelector('.price .price__container .price-item--sale');
       let priceRange = document.getElementById('price-range');
+      select.forEach(function (elem) {
+        elem.addEventListener("click", function (event) {
   
-
-      if (select) {
-        // Set "Bitte auswählen" as the default selected option
-        select.value = "";
-  
-        // Show price range initially since "Bitte auswählen" is selected by default
-        //priceRange.style.display = 'block';
-        priceRegular.style.display = 'none';
-        priceSale.style.display = 'none';
-  
-        select.addEventListener('change', function() {
-            console.log('change');
-          if (select.value === "") {
-              console.log('1');
-         
-            priceRange.classList.remove('hide');
-            priceRegular.style.display = 'none';
-            priceSale.style.display = 'none';
-          } else {
-              console.log('2');
-           
-             priceRange.classList.add('hide');
-            priceRegular.style.display = 'block';
-            priceSale.style.display = 'block';
-          }
+          console.log(event);
+          console.log(select);
         });
-  
-      }
+      });
+
+      
 
 
     }

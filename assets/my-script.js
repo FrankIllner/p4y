6cf63966ffbceccea81ebe3c6e201324e90aa183
FrankIllner <frankillner@web.de>
2024-05-23 14:08:50 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -1,12 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.indexOf("products") > -1) {
-      var select = document.querySelectorAll('.select__select');
-      select.forEach(function (elem) {
-        elem.addEventListener("click", function (event) {
+      let select = document.querySelectorAll('.select__select');
+      let priceRegular = document.querySelector('.price .price__container .price-item--regular');
+      let priceSale = document.querySelector('.price .price__container .price-item--sale');
+      let priceRange = document.getElementById('price-range');
   
-          console.log("Variante wurde gewechselt");
+
+      if (select) {
+        // Set "Bitte auswählen" as the default selected option
+        select.value = "";
+  
+        // Show price range initially since "Bitte auswählen" is selected by default
+        //priceRange.style.display = 'block';
+        priceRegular.style.display = 'none';
+        priceSale.style.display = 'none';
+  
+        select.addEventListener('change', function() {
+            console.log('change');
+          if (select.value === "") {
+              console.log('1');
+         
+            priceRange.classList.remove('hide');
+            priceRegular.style.display = 'none';
+            priceSale.style.display = 'none';
+          } else {
+              console.log('2');
+           
+             priceRange.classList.add('hide');
+            priceRegular.style.display = 'block';
+            priceSale.style.display = 'block';
+          }
         });
-      });
+  
+      }
+
+
     }
   });
 

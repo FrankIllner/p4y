b38d732a4e0b596bd7ca93a14d14ea3b4b43c27b
FrankIllner <frankillner@web.de>
2024-05-23 21:43:23 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -4,8 +4,31 @@ document.addEventListener("DOMContentLoaded", function () {
         let priceRegular = document.querySelector('.product__info-wrapper .price__container .price-item--regular');
         let priceSale = document.querySelector('.product__info-wrapper .price__container .price-item--sale');
         let priceRange = document.querySelector('.product__info-wrapper .price__container .price-range-example');
+        priceRegular.classList.add('foo');
+        if (select) {
+           
+            select.forEach(function (elem) {
+                elem.addEventListener("change", function (event) {
+                    var item = event.target.value;
+                    console.log("Selected value:", item);
 
-       
+                    if (item === "") {
+                        console.log('1');
+
+                        priceRange.classList.remove('hidden');
+                    
+                    
+                        console.log(priceRange);
+                    } else {
+                        console.log('2');
+                        
+                        priceRange.classList.add('hidden');
+                        console.log(priceRange);
+                    }
+
+                });
+            });
+        }
 
     }
 });

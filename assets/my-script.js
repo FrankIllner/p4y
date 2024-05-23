39f5cf2c7f76845b06dfb57cda0453dc11ca6f93
FrankIllner <frankillner@web.de>
2024-05-23 22:34:03 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.indexOf("products") > -1) {
         let select = document.querySelectorAll('.select__select');
         let productWapper = document.querySelector('.product__info-wrapper');
-
+        productWapper.classList.add("is-price-range");
         select.forEach(function (elem) {
             elem.addEventListener("change", function (event) {
                 var item = event.target.value;

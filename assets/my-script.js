6a6f037f102bd514f08a047bc74c6c165004a78c
FrankIllner <frankillner@web.de>
2024-05-23 14:36:12 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -1,23 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.indexOf("products") > -1) {
-      let select = document.querySelector('.select__select');
+      let select = document.querySelectorAll('.select__select');
       let priceRegular = document.querySelector('.product__info-wrapper .price__container .price-item--regular');
       let priceSale = document.querySelector('.product__info-wrapper .price__container .price-item--sale');
       let priceRange = document.querySelector('.product__info-wrapper .price__container .price-range');
-        console.log(select.target.value);
+ 
       select.forEach(function (elem) {
         elem.addEventListener("click", function (event) {
             var item = event.target.value;
             console.log(item);
 
-        if (item == "") {
+        if (item === "") {
             console.log('1');
-       
+            priceRange.classList.remove('hide');
           priceRange.style.display = 'block';
           priceRegular.style.display = 'none';
           priceSale.style.display = 'none';
         } else {
             console.log('2');
+            priceRange.classList.add('hide');
             priceRange.style.display = 'none';
           priceRegular.style.display = 'block';
           priceSale.style.display = 'block';

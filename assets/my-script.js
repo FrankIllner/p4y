ba408cc44d37c2eb06e5aa6ff2ddbea1eb9a9c26
FrankIllner <frankillner@web.de>
2024-05-23 14:37:54 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -12,13 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (item === "") {
             console.log('1');
-            priceRange.classList.remove('hide');
+            priceRange.classList.remove('xxxxx');
           priceRange.style.display = 'block';
           priceRegular.style.display = 'none';
           priceSale.style.display = 'none';
         } else {
             console.log('2');
-            priceRange.classList.add('hide');
+            priceRange.classList.add('xxxxx');
             priceRange.style.display = 'none';
           priceRegular.style.display = 'block';
           priceSale.style.display = 'block';

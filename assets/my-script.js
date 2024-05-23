1afe784a2f8faac0667e2ef5f7047604458f196b
FrankIllner <frankillner@web.de>
2024-05-23 21:17:06 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -5,33 +5,35 @@ document.addEventListener("DOMContentLoaded", function () {
         let priceSale = document.querySelector('.product__info-wrapper .price__container .price-item--sale');
         let priceRange = document.querySelector('.product__info-wrapper .price__container .price-range-example');
 
+        if (select) {
+            select.value = "";
+            select.forEach(function (elem) {
+                elem.addEventListener("change", function (event) {
+                    var item = event.target.value;
+                    console.log("Selected value:", item);
 
-        select.forEach(function (elem) {
-            elem.addEventListener("click", function (event) {
-                var item = event.target.value;
-                console.log("Selected value:", item);
-
-                if (item === "") {
-                    console.log('Show price range');
-                    document.querySelector('.product__info-wrapper .no-js-hidden').classList.remove('hidden');
-                    priceRange.classList.remove('hidden');
-                   
-                    priceRegular.style.display = 'none';
-                    priceSale.style.display = 'none';
-                
-                    console.log(priceRange);
-                } else {
-                    console.log('Show regular/sale prices');
+                    if (item === "") {
+                        console.log('1');
+                        document.querySelector('.product__info-wrapper .no-js-hidden').classList.remove('hidden');
+                        priceRange.classList.remove('hidden');
                     
-                    priceRange.classList.add('hidden');
-                   
-                    priceRegular.style.display = 'block';
-                    priceSale.style.display = 'block';
-                    console.log(priceRange);
-                }
+                        priceRegular.style.display = 'none';
+                        priceSale.style.display = 'none';
+                    
+                        console.log(priceRange);
+                    } else {
+                        console.log('2');
+                        
+                        priceRange.classList.add('hidden');
+                    
+                        priceRegular.style.display = 'block';
+                        priceSale.style.display = 'block';
+                        console.log(priceRange);
+                    }
 
+                });
             });
-        });
+        }
 
     }
 });

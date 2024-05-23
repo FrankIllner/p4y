0ad1b0c4a521e5ceac1138489dcb023d80b33bd8
FrankIllner <frankillner@web.de>
2024-05-23 14:43:14 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -1,36 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.indexOf("products") > -1) {
-      let select = document.querySelectorAll('.select__select');
-      let priceRegular = document.querySelector('.product__info-wrapper .price__container .price-item--regular');
-      let priceSale = document.querySelector('.product__info-wrapper .price__container .price-item--sale');
-      let priceRange = document.querySelector('.product__info-wrapper .price__container .price-range');
- 
-      select.forEach(function (elem) {
-        elem.addEventListener("click", function (event) {
-            var item = event.target.value;
-            console.log(item);
+        let select = document.querySelectorAll('.select__select');
+        let priceRegular = document.querySelector('.product__info-wrapper .price__container .price-item--regular');
+        let priceSale = document.querySelector('.product__info-wrapper .price__container .price-item--sale');
+        let priceRange = document.querySelector('.product__info-wrapper .price__container .price-range');
 
-        if (item === "") {
-            console.log('1');
-            priceRange.classList.remove('xxxxx');
-          priceRange.style.display = 'block';
-          priceRegular.style.display = 'none';
-          priceSale.style.display = 'none';
-        } else {
-            console.log('2');
-            priceRange.classList.add('xxxxx');
-            priceRange.style.display = 'none';
-          priceRegular.style.display = 'block';
-          priceSale.style.display = 'block';
+        if (!priceRange) {
+            console.error("Price range element not found!");
+            return;
         }
-      
+
+        select.forEach(function (elem) {
+            elem.addEventListener("click", function (event) {
+                var item = event.target.value;
+                console.log("Selected value:", item);
+
+                if (item === "") {
+                    console.log('Show price range');
+                    priceRange.classList.remove('hide');
+                    priceRange.style.display = 'block';
+                    priceRegular.style.display = 'none';
+                    priceSale.style.display = 'none';
+                } else {
+                    console.log('Show regular/sale prices');
+                    priceRange.classList.add('hide');
+                    priceRange.style.display = 'none';
+                    priceRegular.style.display = 'block';
+                    priceSale.style.display = 'block';
+                }
+
+            });
         });
-      });
 
-    
     }
-  });
-
-
-  
-
+});

aa4d980a581af5064496c169b52cb40f13587754
FrankIllner <frankillner@web.de>
2024-05-23 14:32:25 +0200

    test

--- assets/my-script.js
+++ assets/my-script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.indexOf("products") > -1) {
-      let select = document.querySelectorAll('.select__select');
-      let priceRegular = document.querySelectorAll('.product__info-wrapper .price__container .price-item--regular');
-      let priceSale = document.querySelectorAll('.product__info-wrapper .price__container .price-item--sale');
-      let priceRange = document.querySelectorAll('.product__info-wrapper .price__container .price-range');
-    console.log(priceRange);
+      let select = document.querySelector('.select__select');
+      let priceRegular = document.querySelector('.product__info-wrapper .price__container .price-item--regular');
+      let priceSale = document.querySelector('.product__info-wrapper .price__container .price-item--sale');
+      let priceRange = document.querySelector('.product__info-wrapper .price__container .price-range');
+        console.log(select);
       select.forEach(function (elem) {
         elem.addEventListener("click", function (event) {
             var item = event.target.value;
